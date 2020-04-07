74d64764e9cac847a2b93c515d0f60ca45023743
MiracleHedgehog <panopticon130@gmail.com>
2020-04-07 16:27:40 +0900

    config.js 作成

---
+++ web/src/config/config.js
@@ -0,0 +1,18 @@
+const config = {
+  BASE_FAVORITE_URL:
+    "https://card.dartslive.com/t/coinstore/theme/theme_favorite_opponent.jsp?card_id=",
+  ENV: "local", // local or firebase
+  DB_LOCAL_HOST: "http://localhost:3000",
+  WEB_LOCAL_HOST: "http://localhost:8080",
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+  },
+};
+
+export default config;
